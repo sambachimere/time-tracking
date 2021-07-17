35eed03aab82ee04cc6dbce63b287bc9375c110f
Samba Diaw <sambachimerediaw@gmail.com>
2021-07-17 06:20:11 +0100

    wip

--- App.js
+++ App.js
@@ -1,19 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import uuidv4 from "uuid/v4";
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import EditableTimer from './components/EditableTimer';
+import ToggleableTimerForm from './components/ToggleableTimerForm';
+
+export default class App extends React.Component {
+  state  = {
+    timers: [
+      {
+        title: "Mow the lawn",
+        project: "House Chores",
+        id: uuidv4(),
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: "Bake squash",
+        project: "Kitchen Chores",
+        id: uuidv4(),
+        elapsed: 1273998,
+        isRunning: false,
+      },
+    ],
+  };
+
+  render() {
+    const { timers } = this.state;
+
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm isOpen={false} />
+          {timers.map(
+            ({ title, project, id, elapsed, isRunning }) => (
+              <EditableTimer 
+                key={id}
+                id={id}
+                title={title}
+                project={project}
+                elapsed={elapsed}
+                isRunning={isRunning}
+              />
+            ),
+          )} 
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#D6D7DA",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
   },
 });
